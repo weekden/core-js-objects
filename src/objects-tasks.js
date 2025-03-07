1a74bbb05156f8e5aecdc6c7ba39172084911f42
DenisNedelko <dnsndlk@gmail.com>
2025-03-07 23:03:35 +0300

    feat: add solution 13 2025-03-07 23:03:35

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -278,8 +278,17 @@ function sortCitiesArray(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+  array.forEach((item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(value);
+  });
+  return map;
 }
 
 /**
