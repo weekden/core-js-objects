47a53c31a2c4c342352454eea1a575818cc06dc5
DenisNedelko <dnsndlk@gmail.com>
2025-03-09 11:24:12 +0300

    feat: add solution 8 2025-03-09 11:24:12

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -159,8 +159,37 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let count25 = 0;
+  let count50 = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    const coin = queue[i];
+    if (coin === 25) {
+      count25 += 1;
+    }
+
+    if (coin === 50) {
+      count50 += 1;
+      if (count25 >= 1) {
+        count25 -= 1;
+      } else {
+        return false;
+      }
+    }
+
+    if (coin === 100) {
+      if (count50 > 0 && count25 > 0) {
+        count50 -= 1;
+        count25 -= 1;
+      } else if (count25 >= 3) {
+        count25 -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
